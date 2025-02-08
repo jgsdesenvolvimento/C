935fcb0546cad9d6a5c3141345f10fac19e9254b
Juliano G Silva <92408535+julianowebdeveloper@users.noreply.github.com>
2025-02-08 14:05:15 -0300

    Solicitando ao usuário a entrada dos dados

--- calculando_media.c
+++ calculando_media.c
@@ -6,7 +6,22 @@ as notas, operadores matemáticos para somar e calcular a média, e operadores d
 
 int mai(){
     //Definindo as variaveis do programa
-    float N1,N2,N3,Media;
+    float N1,N2,N3,N4,Media;
     char Aluno[20];
 
+    //Solicitando ao usuário a entrada dos dados
+    printf("Informe o nome do Aluno: ");
+    scanf("%s",&Aluno);
+    printf("Informe a nota da diciplina Ciências da Natureza e suas Tecnologias: \n");
+    scanf("%f",&N1);
+    printf("Informe a nota da diciplina Ciências Humanas e Sociais Aplicadas: \n");
+    scanf("%f",&N2);
+    printf("Informe a nota da diciplina Linguagens e suas Tecnologias: \n");
+    scanf("%f",&N3);
+    printf("Informe a nota da diciplina Matemática e suas Tecnologias: \n");
+    scanf("%f",&N4);
+
+    //Calculo da Média
+
+    Media = N1 + N2 + N3 + N4 / 4;
 }
