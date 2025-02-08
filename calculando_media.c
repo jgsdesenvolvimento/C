620a138fcd270c82974cbbc8272feaa7a72db9b8
Juliano G Silva <92408535+julianowebdeveloper@users.noreply.github.com>
2025-02-08 13:56:08 -0300

    Definindo as variaveis do programa

---
+++ calculando_media.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+/*Agora você aplicará os conceitos estudados no nível intermediário criando um programa em C 
+que calcula a média de três notas inseridas pelo usuário. Utilizaremos variáveis para armazenar
+as notas, operadores matemáticos para somar e calcular a média, e operadores de atribuição para atualizar*/
+
+int mai(){
+    //Definindo as variaveis do programa
+    float N1,N2,N3,Media;
+    char Aluno[20];
+
+}
