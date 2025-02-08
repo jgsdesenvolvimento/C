ca8a979c3c1c4c4872b0c066b0ba1d763f8c42a3
Juliano G Silva <92408535+julianowebdeveloper@users.noreply.github.com>
2025-02-08 10:43:13 -0300

    comentario

--- conversao_datatype.c
+++ conversao_datatype.c
@@ -10,11 +10,11 @@ int main(){
 
     /*  -- Conversão Implicita: Ocore automaticamente quando se mistura diferentes tipos de dados
     em uma mesma expressão.*/
-    
+
     float Resultado = A + B;
     printf("O resultado da conversão implicita de  %d + %.2f é: %.2f \n",A,B,Resultado);
 
-    /*  -- Conversão Explicita: Ocore quando você força a conversão do tipo de dados na expressão.*/
+    /*  -- Conversão Explicita: Ocore quando você força a conversão do tipo de dados na expressão usando o operador (Casting).*/
 
     float quociente = (float) A / C; // a variável A é explicitamento convertida para Float.
     printf("O resultado da conversão explicita de %d / por %d é de: %.2f \n",A,C,quociente);
