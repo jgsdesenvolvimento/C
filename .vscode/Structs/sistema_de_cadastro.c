8766f56e74bf127718f4afc8cf3fa4c91688c82c
Juliano G Silva <92408535+julianowebdeveloper@users.noreply.github.com>
2025-02-08 17:51:08 -0300

    Criação das Structs

--- .vscode/Structs/sistema_de_cadastro.c
+++ .vscode/Structs/sistema_de_cadastro.c
@@ -1,2 +1,20 @@
 #include <stdio.h>
 
+typedef struct {
+    char Name[50];
+    float Price;
+    int Quantity;
+}Product;
+
+typedef struct {
+    int Id;
+    Product Products[99];
+    float Total;
+}Order;
+
+
+int main(){
+
+
+    return 0;
+}
