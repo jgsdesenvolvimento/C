ad4798e824d70357d56b250e31bbb44f3c846458
Juliano G Silva <92408535+julianowebdeveloper@users.noreply.github.com>
2025-02-15 16:37:44 -0300

    Criação e declarção das variaveis

---
+++ .vscode/Operadores Relacionai/operadores_relacionais.c
@@ -0,0 +1,70 @@
+#include <stdio.h>
+
+int main(){
+
+    //Declarando as váriaveis
+    int X,Y;
+
+    printf("Informe o primeiro número: \n");
+    scanf("%d", &X);
+    printf("Informe o segundo númeoro: \n");
+    scanf("%d", &Y);
+
+    printf("%d é maior que %d ?\n",X,Y);
+
+    if (X > Y)
+    {
+        printf("Sim. \n");
+    }
+
+    else printf("Não. \n");
+
+    printf("%d é menor que %d ?\n",X,Y);
+
+    if (X < Y)
+    {
+        printf("Sim. \n");
+    }
+
+    else printf("Não. \n");
+
+    printf("%d é maior ou igual a %d ?\n",X,Y);
+
+    if (X >= Y)
+    {
+        printf("Sim. \n");
+    }
+
+    else printf("Não. \n");
+
+    printf("%d é menor ou igual a %d ?\n",X,Y);
+
+    if (X <= Y)
+    {
+        printf("Sim. \n");
+    }
+
+    else printf("Não. \n");
+
+    printf("%d é igual a %d ?\n",X,Y);
+
+    if (X == Y)
+    {
+        printf("Sim. \n");
+    }
+
+    else printf("Não. \n");
+
+    printf("%d é diferente de %d ?\n",X,Y);
+
+    if (X != Y)
+    {
+        printf("Sim. \n");
+    }
+
+    else printf("Não. \n");
+
+    
+    //printf("RESPOSTA:1 para verdadeiro e 0 para falso:%d \n", X > Y);
+
+}
